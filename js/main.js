6492c085a4aba8701ca27cf5e01d62e1c86215d7
end0cr1ne <f.warren210@gmail.com>
2016-02-13 16:50:15 +0530

    Events

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
     'use strict';
 
-    var support = { transitions: Modernizr.csstransitions },
+    var support = { transitions: Modernizr.csstransitions, animations : Modernizr.cssanimations },
     // transition end event name
         transEndEventNames = { 'WebkitTransition': 'webkitTransitionEnd', 'MozTransition': 'transitionend', 'OTransition': 'oTransitionEnd', 'msTransition': 'MSTransitionEnd', 'transition': 'transitionend' },
         transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
@@ -21,6 +21,23 @@
                 onEndCallbackFn();
             }
         },
+        animEndEventNames = { 'WebkitAnimation' : 'webkitAnimationEnd', 'OAnimation' : 'oAnimationEnd', 'msAnimation' : 'MSAnimationEnd', 'animation' : 'animationend' },
+        animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ],
+        onEndAnimation = function( el, callback ) {
+            var onEndCallbackFn = function( ev ) {
+                if( support.animations ) {
+                    if( ev.target != this ) return;
+                    this.removeEventListener( animEndEventName, onEndCallbackFn );
+                }
+                if( callback && typeof callback === 'function' ) { callback.call(); }
+            };
+            if( support.animations ) {
+                el.addEventListener( animEndEventName, onEndCallbackFn );
+            }
+            else {
+                onEndCallbackFn();
+            }
+        },
     // the pages wrapper
         stack = document.querySelector('.pages-stack'),
     // the page elements
@@ -28,7 +45,7 @@
     // total number of page elements
         pagesTotal = pages.length,
     // index of current page
-        current = 0,
+        current = 1,
     // menu button
         menuCtrl = document.querySelector('button.menu-button'),
     // the navigation wrapper
@@ -41,6 +58,7 @@
     function init() {
         buildStack();
         initEvents();
+        init2();
     }
 
     function buildStack() {
@@ -204,6 +222,110 @@
         }
 
         return idxs;
+    }
+
+    // from http://www.sberry.me/articles/javascript-event-throttling-debouncing
+    function throttle(fn, delay) {
+        var allowSample = true;
+
+        return function(e) {
+            if (allowSample) {
+                allowSample = false;
+                setTimeout(function() { allowSample = true; }, delay);
+                fn(e);
+            }
+        };
+    }
+
+    // sliders - flickity
+    var sliders = [].slice.call(document.querySelectorAll('.slider')),
+    // array where the flickity instances are going to be stored
+        flkties = [],
+    // grid element
+        grid = document.querySelector('.grid'),
+    // isotope instance
+        iso,
+    // filter ctrls
+        filterCtrls = [].slice.call(document.querySelectorAll('.filter > button')),
+    // cart
+        cart = document.querySelector('.cart'),
+        cartItems = cart.querySelector('.cart__count');
+
+    function init2() {
+        // preload images
+        imagesLoaded(grid, function() {
+            initFlickity();
+            initIsotope();
+            initEvents2();
+            classie.remove(grid, 'grid--loading');
+        });
+    }
+
+    function initFlickity() {
+        sliders.forEach(function(slider){
+            var flkty = new Flickity(slider, {
+                prevNextButtons: false,
+                wrapAround: true,
+                cellAlign: 'center',
+                contain: true,
+                resize: false
+            });
+
+            // store flickity instances
+            flkties.push(flkty);
+        });
+    }
+
+    function initIsotope() {
+        iso = new Isotope( grid, {
+            isResizeBound: false,
+            itemSelector: '.grid__item',
+            percentPosition: true,
+            masonry: {
+                // use outer width of grid-sizer for columnWidth
+                columnWidth: '.grid__sizer'
+            },
+            transitionDuration: '0.6s'
+        });
+    }
+
+    function initEvents2() {
+        filterCtrls.forEach(function(filterCtrl) {
+            filterCtrl.addEventListener('click', function() {
+                classie.remove(filterCtrl.parentNode.querySelector('.filter__item--selected'), 'filter__item--selected');
+                classie.add(filterCtrl, 'filter__item--selected');
+                iso.arrange({
+                    filter: filterCtrl.getAttribute('data-filter')
+                });
+                recalcFlickities();
+                iso.layout();
+            });
+        });
+
+        // window resize / recalculate sizes for both flickity and isotope/masonry layouts
+        window.addEventListener('resize', throttle(function(ev) {
+            recalcFlickities()
+            iso.layout();
+        }, 50));
+
+        // add to cart
+        [].slice.call(grid.querySelectorAll('.grid__item')).forEach(function(item) {
+            item.querySelector('.action--buy').addEventListener('click', addToCart);
+        });
+    }
+
+    function addToCart() {
+        classie.add(cart, 'cart--animate');
+        setTimeout(function() {cartItems.innerHTML = Number(cartItems.innerHTML) + 1;}, 200);
+        onEndAnimation(cartItems, function() {
+            classie.remove(cart, 'cart--animate');
+        });
+    }
+
+    function recalcFlickities() {
+        for(var i = 0, len = flkties.length; i < len; ++i) {
+            flkties[i].resize();
+        }
     }
 
     init();
